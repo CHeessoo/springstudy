99b322e2e0c26c61148fe1d6dd0e080f9711cf4d
CHeessoo <gabycho41@gmail.com>
2023-10-06 11:56:36 +0900

    20231006

---
+++ 07_ajax/src/main/java/com/gdu/app07/controller/AjaxController4.java
@@ -0,0 +1,43 @@
+package com.gdu.app07.controller;
+
+import java.io.File;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class AjaxController4 {
+
+  // 서비스 생략하고 컨트롤러에서 직접 반환
+  
+  
+  @GetMapping(value="/ajax4/display.do", produces="application/octet-stream")  // octet-stream : file 자체의 데이터 타입
+  public ResponseEntity<byte[]> display(@RequestParam("path") String path
+                                      , @RequestParam("filename") String filename) {
+    
+    ResponseEntity<byte[]> responseEntity = null;
+    
+    try {
+      
+      // File 객체
+      File file = new File(path, filename);
+      
+      // File 객체를 byte[]로 변환하는 Spring 클래스
+      byte[] b = FileCopyUtils.copyToByteArray(file);
+      
+      // ResponseEntity 객체 생성
+      responseEntity = new ResponseEntity<byte[]>(b, HttpStatus.OK);
+      
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  
+    return responseEntity;
+    
+  }
+    
+}
