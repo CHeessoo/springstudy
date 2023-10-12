7c914622f8017197020fc5b02df61f50f3406211
CHeessoo <gabycho41@gmail.com>
2023-10-12 10:35:40 +0900

    20231012

--- 11_mybatis_mockmvc/src/main/java/com/gdu/app11/dao/ContactDao.java
+++ 11_mybatis_mockmvc/src/main/java/com/gdu/app11/dao/ContactDao.java
@@ -1,12 +1,8 @@
 package com.gdu.app11.dao;
 
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
 import java.util.List;
 
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.PreparedStatementSetter;
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
 
 import com.gdu.app11.dto.ContactDto;
@@ -17,26 +13,15 @@ import lombok.RequiredArgsConstructor;
 @Repository
 public class ContactDao {
   
-  private final JdbcTemplate jdbcTemplate;
+  private final SqlSessionTemplate sqlSessionTemplate;  // Bean으로 만들어 두었기 때문에 @RequiredArgsConstructor에 의해 자동으로 @Autowired가 됨
 
   /**
    * 삽입 메소드<br>
    * @param contactDto 삽입할 연락처 정보(name, tel, email, address)
    * @return insertCount 삽입된 행(Row)의 개수, 1이면 삽입 성공, 0이면 삽입 실패
    */
-  public int insert(final ContactDto contactDto) {  // 매개변수 변조를 막기 위해서 final 추가
-    String sql = "INSERT INTO CONTACT_T(CONTACT_NO, NAME, TEL, EMAIL, ADDRESS, CREATED_AT) VALUES(CONTACT_SEQ.NEXTVAL, ?, ?, ?, ?, TO_CHAR(SYSDATE, 'YYYY-MM-DD HH24:MI:SS'))";
-    int insertCount = jdbcTemplate.update(sql, new PreparedStatementSetter() {
-      @Override
-      public void setValues(PreparedStatement ps) throws SQLException {
-       // ?(물음표)에 값 채워주기
-       ps.setString(1, contactDto.getName());
-       ps.setString(2, contactDto.getTel());
-       ps.setString(3, contactDto.getEmail());
-       ps.setString(4, contactDto.getAddress());
-      }
-    });
-    return insertCount;
+  public int insert(ContactDto contactDto) {  
+    return sqlSessionTemplate.insert("mybatis.mapper.contact.insert", contactDto); // insert(호출할id전달, 호출할parameter전달) : namespace(mapper경로)와 id를 함께 적어서 mapper구분을 해주어야 한다.
   }
   
   /**
@@ -44,19 +29,8 @@ public class ContactDao {
    * @param contactDto 수정할 연락처 정보(contact_no, name, tel, email, address)
    * @return updateCount 수정된 행(Row)의 개수, 1이면 수정 성공, 0이면 수정 실패
    */
-  public int update(final ContactDto contactDto) {
-    String sql = "UPDATE CONTACT_T SET NAME = ?, TEL = ?, EMAIL = ?, ADDRESS = ? WHERE CONTACT_NO = ?";
-    int updateCount = jdbcTemplate.update(sql, new PreparedStatementSetter() {
-      @Override
-      public void setValues(PreparedStatement ps) throws SQLException {
-        ps.setString(1, contactDto.getName());
-        ps.setString(2, contactDto.getTel());
-        ps.setString(3, contactDto.getEmail());
-        ps.setString(4, contactDto.getAddress());
-        ps.setInt(5, contactDto.getContact_no());
-      }
-    });
-    return updateCount;
+  public int update(ContactDto contactDto) {
+    return sqlSessionTemplate.update("mybatis.mapper.contact.update", contactDto);  // update(호출할 쿼리의 id, parameter)
   }
   
   /**
@@ -64,24 +38,17 @@ public class ContactDao {
    * @param contact_no 삭제할 연락처 번호
    * @return deleteCount 삭제된 행(Row)의 개수, 1이면 삭제 성공, 0이면 삭제 실패
    */
-  public int delete(final int contact_no) {
-    String sql = "DELETE FROM CONTACT_T WHERE CONTACT_NO = ?";
-    int deleteCount = jdbcTemplate.update(sql, new PreparedStatementSetter() {
-      @Override
-      public void setValues(PreparedStatement ps) throws SQLException {
-        ps.setInt(1, contact_no);
-      }
-    });
-    return deleteCount;
+  public int delete(final int contactNo) {
+    return sqlSessionTemplate.delete("mybatis.mapper.contact.delete", contactNo);  // delete(id, param)
   }
   
   /**
    * 전체 조회 메소드<br>
    * @return  조회된 모든 연락처 정보(ContactDto)
    */
-  public List<ContactDto> selectList() {
-    String sql = "SELECT CONTACT_NO, NAME, TEL, EMAIL, ADDRESS, CREATED_AT FROM CONTACT_T ORDER BY CONTACT_NO";
-    return jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(ContactDto.class));
+  public List<ContactDto> selectList() {  
+    // select를 전달하는 쿼리는 selectList와 selectOne이 있다. 목록을 전달할것이기 때문에 selectList를 사용.
+    return sqlSessionTemplate.selectList("mybatis.mapper.contact.selectList");  // id만 전달
   }
   
   /**
@@ -89,9 +56,8 @@ public class ContactDao {
    * @param contact_no 조회할 연락처 번호
    * @return contactDto 조회된 연락처 정보, 조회된 연락처가 없으면 null 반환
    */
-  public ContactDto selectContactByNo(final int contact_no) { 
-    String sql = "SELECT CONTACT_NO, NAME, TEL, EMAIL, ADDRESS, CREATED_AT FROM CONTACT_T WHERE CONTACT_NO = ?";
-    return jdbcTemplate.queryForObject(sql, new BeanPropertyRowMapper<>(ContactDto.class), contact_no);
+  public ContactDto selectContactByNo(int contactNo) { 
+    return sqlSessionTemplate.selectOne("mybatis.mapper.contact.selectContactByNo", contactNo);  // 전달할 것이 1개 이기 때문에 selectOne사용
   }
   
 }
